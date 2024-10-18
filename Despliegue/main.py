fcd7bedf39cdc95bb21162d2f643c6af0889b165
gluoneros <gluoneros@gmail.com>
2024-10-17 22:06:12 -0500

    unirprogreso

---
+++ Despliegue/main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+import pickle
+
+
+app = Flask(__name__)
+
+
+with open('modelo2.pkl', 'rb') as f:
+    modelo2 = pickle.load(f)
+    
+@app.route('/')
+def home():
+    return render_template('index.html')
